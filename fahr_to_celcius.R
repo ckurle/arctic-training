a0506d6510550767173dea2ff55e5540b82789ca
Carolyn Kurle <ckurle@ucsd.edu>
2019-01-17 16:11:07 -0800

    Temperature Practice but it's not working so I need to get help.

---
+++ fahr_to_celcius.R
@@ -0,0 +1,13 @@
+airtemps <- c(212, 33.5, 90, 100, 0, -40)
+
+celsius1 <- (airtemps[1]-32)*5/9
+celsius2 <- (airtemps[2]-32)*5/9
+celsius3 <- (airtemps[3]-32)*5/9
+
+fahr_to_celsius <- function(fahr){
+    celsius <- (fahr-32)*5/9
+    return(celsius)
+}
+
+celsius4 <- fahr_to_celsius(airtemps[1])
+celsius1 == celsius4
